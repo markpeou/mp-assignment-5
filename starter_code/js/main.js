e9eaf52d15b4f8e8a5fdd5b74813904a1712ca30
markpeou <markpeou@gmail.com>
2017-10-04 19:53:00 +1100

    updated!

--- starter_code/js/main.js
+++ starter_code/js/main.js
@@ -5,27 +5,40 @@ function changeBackground(event) {
   var userInput = $('#city-type').val();
   // if city is "nyc" or "new york" or "new york city.
   //Have allowed for Capitalisation as well as all caps for abbreviated forms in all cities.
-
-  if (userInput === 'nyc' || userInput === 'new york' ||userInput === 'new york city'|| userInput === 'NYC' || userInput === 'New York' || userInput === 'New York City') {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'nyc' || userInput === 'new york' || userInput === 'new york city') {
+    clearBackground();
     $('body').addClass('nyc')
+    $('#city-type').val('');
 
 // If city is "sf" or "san francisco" or "bay area"
-
-} else if (userInput ==='sf' ||userInput ==='san francisco' ||userInput === 'bay area' || userInput === 'SF' || userInput === 'Bay Area' || userInput === 'San Francisco') {
+} else if (userInput === 'sf' || userInput === 'san francisco' || userInput === 'bay area' ) {
+  clearBackground();
   $('body').addClass('sf')
+  $('#city-type').val('');
 
   // if city is "LA" or "Los Angeles" or "LAX"
-} else if (userInput === 'LA' ||userInput ==='los angeles' || userInput === 'LAX' || userInput === 'la' ||userInput === 'Los Angeles' || userInput === 'lax' ) {
+} else if (userInput === 'la' || userInput === 'los angeles' || userInput === 'lax' ) {
+  clearBackground();
   $('body').addClass ('la')
+  $('#city-type').val('');
 
   // if city is "Austin" or "ATX"
-} else if(userInput === 'Austin' || userInput === 'ATX' || userInput === 'austin'|| userInput === 'atx') {
+} else if(userInput === 'austin' || userInput === 'atx' ) {
+  clearBackground();
   $('body').addClass('austin')
+  $('#city-type').val('');
 
   //if city is "sydney" or "syd"
-} else if (userInput === 'sydney' || userInput === 'syd' || userInput === 'Sydney' || userInput === 'SYD') {
+} else if (userInput === 'sydney' || userInput === 'syd' ) {
+  clearBackground();
   $('body').addClass('sydney')
+  $('#city-type').val('');
 }
+}
+
+function clearBackground(){
+  $('body').removeClass('nyc sf la austin sydney')
 }
 // $('.jq_noSpaces').on('change', function(){
-//     $(this).val($(this).val().replace(/\s+/g,""))}
+//     $('#city-type').val($(this).val().replace(/\s+/g,""))}
